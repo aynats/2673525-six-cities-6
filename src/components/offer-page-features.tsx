406e1261c9c008cf9a0eb5b9a69a48f106bcfe8c
Tatiana <tanya-sok-05@mail.ru>
2025-12-05 19:44:11 +0500

    offer-bookmark & offer-page moved to special files

---
+++ src/components/offer-page-features.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type OfferBookmarkButtonProps = {
+  type: string;
+  bedrooms: number;
+  maxAdults: number;
+};
+
+function OfferFeatures({ type, bedrooms, maxAdults }: OfferBookmarkButtonProps): JSX.Element {
+    return (
+        <ul className='offer__features'>
+        <li className='offer__feature offer__feature--entire'>
+          {type}
+        </li>
+        <li className='offer__feature offer__feature--bedrooms'>
+          {bedrooms} {bedrooms !== 1 ? 'Bedrooms' : 'Bedroom'}
+        </li>
+        <li className='offer__feature offer__feature--adults'>
+          Max {maxAdults} adults
+        </li>
+      </ul>
+    )
+}
+
+const MemoizedOfferFeatures = React.memo(OfferFeatures);
+MemoizedOfferFeatures.displayName = 'OfferFeatures';
+
+export default MemoizedOfferFeatures;
