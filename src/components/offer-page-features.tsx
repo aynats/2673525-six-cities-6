cef92cbb26e78be6baef1dcd43c44617595a628d
Tatiana <tanya-sok-05@mail.ru>
2025-12-05 20:14:07 +0500

    eslint features

--- src/components/offer-page-features.tsx
+++ src/components/offer-page-features.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 type OfferBookmarkButtonProps = {
   type: string;
@@ -7,19 +7,19 @@ type OfferBookmarkButtonProps = {
 };
 
 function OfferFeatures({ type, bedrooms, maxAdults }: OfferBookmarkButtonProps): JSX.Element {
-    return (
-        <ul className='offer__features'>
-        <li className='offer__feature offer__feature--entire'>
-          {type}
-        </li>
-        <li className='offer__feature offer__feature--bedrooms'>
-          {bedrooms} {bedrooms !== 1 ? 'Bedrooms' : 'Bedroom'}
-        </li>
-        <li className='offer__feature offer__feature--adults'>
-          Max {maxAdults} adults
-        </li>
-      </ul>
-    )
+  return (
+    <ul className='offer__features'>
+      <li className='offer__feature offer__feature--entire'>
+        {type}
+      </li>
+      <li className='offer__feature offer__feature--bedrooms'>
+        {bedrooms} {bedrooms !== 1 ? 'Bedrooms' : 'Bedroom'}
+      </li>
+      <li className='offer__feature offer__feature--adults'>
+        Max {maxAdults} adults
+      </li>
+    </ul>
+  );
 }
 
 const MemoizedOfferFeatures = React.memo(OfferFeatures);
